dd4d11a090532ae34be6aeadb80b5209889cbd77
Gavin-Erisa <gavinmcguire95@gmail.com>
2023-12-29 15:07:02 -0700

    Deploy github workflow.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vite-deploy/",
   assetsInclude: ['*.png'],
   plugins: [react({
     template: {
